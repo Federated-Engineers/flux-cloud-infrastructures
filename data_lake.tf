3b945469007a5ad7eb2372a7575a0aaeb83b0a1b
RofiatAbdulkareem <oyizakareem9@gmail.com>
2026-02-22 15:13:29 +0100

    testing atlantis

--- data_lake.tf
+++ data_lake.tf
@@ -1,3 +1,7 @@
 resource "aws_s3_bucket" "federated_flux_demo" {
   bucket = "federated-engineers-flux-test-bucket1"
 }
+
+resource "aws_s3_bucket" "federated_flux_demo1" {
+  bucket = "federated-engineers-flux-test-bucket2"
+}
