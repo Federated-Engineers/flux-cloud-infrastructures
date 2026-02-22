d0b372a514971e18579c329222a6f87a1dd65605
RofiatAbdulkareem <oyizakareem9@gmail.com>
2026-02-22 14:52:02 +0100

    testing atlantis

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "federated-engineers-terraform-state"
-    key    = "production/terraform.tfstate"
+    key    = "production/flux/terraform.tfstate"
     region = "eu-central-1"
   }
 }
