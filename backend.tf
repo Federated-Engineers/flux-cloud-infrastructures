8ff8e62ef85187c75f328aa2de38c281f7f766db
Akinmeji_Olalekan <akinmejiolalekan7@gmail.com>
2026-02-28 00:36:27 +0100

    solved error

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "federated-flux-staging-bucket"
-    key    = "statefile/remote"
-    region = "eu-central-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "federated-flux-staging-bucket"
+#     key    = "statefile/remote"
+#     region = "eu-central-1"
+#   }
+# }
