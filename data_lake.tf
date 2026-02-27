2a83b01e608c5c39627744123c0e4ad83581ff26
RofiatAbdulkareem <oyizakareem9@gmail.com>
2026-02-27 14:54:27 +0100

    created staging bucket and a staging user

--- data_lake.tf
+++ data_lake.tf
@@ -1,7 +1,7 @@
-resource "aws_s3_bucket" "federated_flux_demo" {
-  bucket = "federated-engineers-flux-test-bucket1"
-}
-
-resource "aws_s3_bucket" "federated_flux_demo1" {
-  bucket = "federated-engineers-flux-test-bucket2"
+resource "aws_s3_bucket" "federated_flux_staging_bucket" {
+  bucket = "federated-flux-staging-bucket"
+    tags = merge(local.common_tags, {
+      Name        = "flux_staging_bucket"
+    }
+)
 }
---
+++ iam.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_user" "federated_flux_staging_user" {
+  name = "flux_staging_user"
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_access_key" "flux_access_key" {
+  user = aws_iam_user.federated_flux_staging_user.name
+}
+
+resource "aws_ssm_parameter" "flux_access" {
+  name  = "/staging/flux/aws-access-key"
+  type  = "String"
+  value = aws_iam_access_key.flux_access_key.id
+}
+
+resource "aws_ssm_parameter" "flux_access_secret" {
+  name  = "/staging/flux/aws-access-secret-key"
+  type  = "String"
+  value = aws_iam_access_key.flux_access_key.secret
+}
+
+resource "aws_iam_user_policy" "flux_staging_user_policy" {
+  name = "flux_staging_user_policy"
+  user = aws_iam_user.federated_flux_staging_user.name
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:PutObject"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:s3:::federated_flux_staging_bucket"
+      },
+    ]
+  })
+}
