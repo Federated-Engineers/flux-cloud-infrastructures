05eb97147f238ea03e0aeae5eb0c610874df038e
Rofiat <oyizakareem9@gmail.com>
2026-03-08 16:56:53 +0100

    Revert "Revert "added lifecycle policy""

--- eu-central-1/production/ecr.tf
+++ eu-central-1/production/ecr.tf
@@ -8,3 +8,26 @@ resource "aws_ecr_repository" "flux_airflow" {
 
   tags = merge(local.common_tags, { Name = "flux-airflow" })
 }
+
+resource "aws_ecr_lifecycle_policy" "flux_airflow_policy" {
+  repository = aws_ecr_repository.flux_airflow.name
+
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Expire images but keep the last 2 untagged images",
+      "selection": {
+        "tagStatus": "untagged",
+        "countType": "imageCountMoreThan",
+        "countNumber": 2
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
