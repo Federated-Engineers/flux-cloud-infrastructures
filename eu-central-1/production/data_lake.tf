a7ded54538ad8439d85037c0a1c92c592efb0413
RofiatAbdulkareem <oyizakareem9@gmail.com>
2026-03-21 19:37:59 +0100

    testing atlantis

--- eu-central-1/production/data_lake.tf
+++
@@ -1,8 +0,0 @@
-module "atlantis_test" {
-  source          = "../modules/s3-bucket"
-  team            = "flux"
-  bucket-use-case = "data-lake"
-  service         = "atlantis-test"
-  versioning      = "Enabled"
-  environment     = var.environment
-}
