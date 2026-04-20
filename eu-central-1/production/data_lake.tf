c03b56936cb829abccdb75083fb734c8aa0801e4
Chichi126 <chinyere.nwigwe126@gmail.com>
2026-04-20 21:18:27 +0100

    edit done

--- eu-central-1/production/data_lake.tf
+++ eu-central-1/production/data_lake.tf
@@ -1,8 +1,8 @@
 module "riveira_bucket" {
-  source          = "../modules/s3_bucket"
+  source          = "../modules/s3-bucket"
   bucket-use-case = "riveira-dataset"
   versioning      = "Enabled"
-  team            = "flux-cloud-infrastructures"
-  environment     = "prod"
+  team            = "flux"
+  environment = "production"
   service         = "data-lake"
 }
