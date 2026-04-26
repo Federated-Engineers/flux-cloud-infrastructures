c2e8c5c250c056db32e4d8c639f7bc9a2796e6f8
Chichi126 <chinyere.nwigwe126@gmail.com>
2026-04-26 21:54:18 +0100

    added glue database

--- eu-central-1/production/data_lake.tf
+++ eu-central-1/production/data_lake.tf
@@ -1,11 +1,3 @@
-module "nordic_s3_bucket" {
-  source          = "../modules/s3-bucket"
-  team            = var.team
-  environment     = var.environment
-  bucket-use-case = "nordic"
-  service         = "flux-airflow"
-  versioning      = "Enabled"
-}
 
 module "riveira_bucket" {
   source          = "../modules/s3-bucket"
