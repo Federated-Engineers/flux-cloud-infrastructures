f806f4646731fd4a3dc29bc9fa1869488cd6b8ce
Rukayat Jimoh <rukayatjimoh@Rukayats-MBP.local>
2026-04-29 20:05:33 +0200

    neuralNest s3 bucket provisioning

--- eu-central-1/production/data_lake.tf
+++ eu-central-1/production/data_lake.tf
@@ -7,3 +7,14 @@ module "nordic_s3_bucket" {
   service         = "flux-airflow"
   versioning      = "Enabled"
 }
+
+
+module "neuralNest_s3_bucket" {
+  source = "../modules/s3-bucket"
+
+  team            = var.team
+  environment     = var.environment
+  bucket-use-case = "neuralNest"
+  service         = "flux-airflow"
+  versioning      = "Enabled"
+}
