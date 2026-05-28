914716d89f1db9a11d4e820735cc5daa4fba7418
Blessing Ekwere <blessingekwere15@gmail.com>
2026-05-28 21:14:04 +0100

    fix terraform fmt formatting

--- eu-central-1/production/gluee_.tf
+++ eu-central-1/production/gluee_.tf
@@ -7,14 +7,6 @@ resource "aws_glue_catalog_table" "veld_vine_catalog_table" {
   name          = "veld-vine-catalog-table"
   database_name = aws_glue_catalog_database.veld_vine_catalog_database.name
 
-  table_type = "EXTERNAL_TABLE"
-
-    parameters = {
-    EXTERNAL              = "TRUE"
-    "parquet.compression" = "SNAPPY"
-  }
-
-
   storage_descriptor {
     location      = "s3://veld-vine-s3/silver/"
     input_format  = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
