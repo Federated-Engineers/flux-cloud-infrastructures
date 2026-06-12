b7e3fd6ce31e49cb3b67ddccc106556af6635019
Chichi126 <chinyere.nwigwe126@gmail.com>
2026-06-12 17:16:29 +0100

    initial repo

--- eu-central-1/production/data_lake.tf
+++ eu-central-1/production/data_lake.tf
@@ -27,3 +27,13 @@ module "neuralnest_s3_bucket" {
   service         = "flux-airflow"
   versioning      = "Enabled"
 }
+
+module "riveira_bucket" {
+  source = "../modules/s3-bucket"
+
+  bucket-use-case = "riveira-dataset"
+  versioning      = "Enabled"
+  team            = "flux"
+  environment     = "production"
+  service         = "data-lake"
+}
